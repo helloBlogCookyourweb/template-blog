0b6c814af219bce1585683cd82f73e16caf97049
unknown <hello.blogcookyourweb.com>
2021-07-23 14:32:00 +0200

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb202b4fd0471744fe7eea35aabd00f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b06d83e9-a35b-4cb5-84f9-c2e91ed8f0e1'
+};
